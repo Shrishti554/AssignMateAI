c6fc9a364c2e44b1a834818432d3e1aee5add1b6
Shrishti Arya <shrishtiarya554@gmail.com>
2025-07-27 16:48:34 +0530

    create background workers with inngest

---
+++ controllers/user.js
@@ -0,0 +1,6 @@
+import brcypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import user from "../models/user.js";
+import {inngest} from "../inngest/client.js";
+
+export const signup = async (req, res) => {}
