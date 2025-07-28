74cbf9289dce74546d521eec0e928e24ba8e7340
Shrishti Arya <shrishtiarya554@gmail.com>
2025-07-28 18:46:11 +0530

    Building tickets controller and inngest middlewares

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import cros from "cros";
 import userRoutes from "./routes/user.js";
+import {serve} from "inngest/express";
+import ticketRoutes from "./routes/ticket.js";
+import {inngest} from "./inngest/client.js";
+import {onTicketCreate} from "./inngest/functions/on-ticket-create.js";
+import {onUserSignup} from "./inngest/functions/on-signup.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -11,6 +16,12 @@ app.use(cros())
 app.use(express.json())
 
 app.use("/api/auth", userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+app.use("/api/inngest", serve({
+    client:inngest,
+    functions: [onTicketCreate, onUserSignup],
+}));
 
 mongoose
      .connect(process.env.MONGODB_URI)
--- routes/ticket.js
+++ routes/ticket.js
@@ -1,6 +1,11 @@
 import express from "express";
+import { authenticate } from "../middlewares/auth.js";
+import { createTicket, getTicket, getTickets } from "../controllers/ticket.js";
 
-const ticket = express.Router();
+const router = express.Router();
 
+router.get("/", authenticate, getTickets);
+router.get("/:id", authenticate, getTicket);
+router.post("/", authenticate, createTicket);
 
-export default ticket;
+export default router;
